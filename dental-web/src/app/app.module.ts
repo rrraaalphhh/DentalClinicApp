6e042361d99f7155d170041c4a087e74562ed2a9
NAD <mronalddavid@gmail.com>
2021-09-16 21:46:01 +0800

    dental-web initial commit

---
+++ dental-web/src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { NavbarComponent } from './shared/navbar/navbar.component';
+import { SidebarComponent } from './shared/sidebar/sidebar.component';
+import { FooterComponent } from './shared/footer/footer.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    DashboardComponent,
+    NavbarComponent,
+    SidebarComponent,
+    FooterComponent,
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
